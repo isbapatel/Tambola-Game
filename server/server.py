39032d2628f5b09ec62396b9faae56b3c43a1d33
isbapatel <isbapatel1205@gmail.com>
2026-01-06 15:11:33 +0530

    js static 3

--- server/server.py
+++ server/server.py
@@ -7,14 +7,29 @@ import websockets
 
 PORT = int(os.environ.get("PORT", 10000))
 
-rooms = {}  # room_id -> { "players": [], "sockets": [] }
+rooms = {}  
+# room_id -> {
+#   players: [],
+#   sockets: [],
+#   tickets: {}   # player_name -> ticket
+# }
 
 def generate_room_id():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 
-async def broadcast(room_id, message):
-    for ws in rooms[room_id]["sockets"]:
-        await ws.send(json.dumps(message))
+
+def generate_ticket():
+    ticket = [[0]*9 for _ in range(3)]
+
+    for row in range(3):
+        cols = random.sample(range(9), 5)
+        for col in cols:
+            start = col * 10 + 1
+            end = 90 if col == 8 else start + 9
+            ticket[row][col] = random.randint(start, end)
+
+    return ticket
+
 
 async def handler(ws):
     room_id = None
@@ -33,7 +48,8 @@ async def handler(ws):
 
                 rooms[room_id] = {
                     "players": [player_name],
-                    "sockets": [ws]
+                    "sockets": [ws],
+                    "tickets": {}
                 }
 
                 await ws.send(json.dumps({
@@ -41,10 +57,7 @@ async def handler(ws):
                     "data": { "room_id": room_id }
                 }))
 
-                await broadcast(room_id, {
-                    "type": "PLAYERS_UPDATE",
-                    "data": { "players": rooms[room_id]["players"] }
-                })
+                await broadcast_players(room_id)
 
             # -------- JOIN ROOM --------
             elif msg_type == "JOIN_ROOM":
@@ -61,36 +74,52 @@ async def handler(ws):
                 rooms[room_id]["players"].append(player_name)
                 rooms[room_id]["sockets"].append(ws)
 
-                # 🔥 IMPORTANT PART
-                await broadcast(room_id, {
-                    "type": "PLAYERS_UPDATE",
-                    "data": { "players": rooms[room_id]["players"] }
-                })
+                await broadcast_players(room_id)
 
             # -------- START GAME --------
             elif msg_type == "START_GAME":
-                await broadcast(room_id, {
-                    "type": "GAME_STARTED",
-                    "data": {}
-                })
+                room = rooms[room_id]
+
+                # generate tickets
+                for i, player in enumerate(room["players"]):
+                    ticket = generate_ticket()
+                    room["tickets"][player] = ticket
+
+                    await room["sockets"][i].send(json.dumps({
+                        "type": "TICKET_ASSIGNED",
+                        "data": { "ticket": ticket }
+                    }))
+
+                # notify all game started
+                for s in room["sockets"]:
+                    await s.send(json.dumps({
+                        "type": "GAME_STARTED",
+                        "data": {}
+                    }))
 
     except websockets.ConnectionClosed:
         pass
 
     finally:
-        # -------- CLEANUP --------
         if room_id and room_id in rooms and ws in rooms[room_id]["sockets"]:
             idx = rooms[room_id]["sockets"].index(ws)
             rooms[room_id]["sockets"].pop(idx)
             rooms[room_id]["players"].pop(idx)
 
             if rooms[room_id]["sockets"]:
-                await broadcast(room_id, {
-                    "type": "PLAYERS_UPDATE",
-                    "data": { "players": rooms[room_id]["players"] }
-                })
+                await broadcast_players(room_id)
             else:
                 del rooms[room_id]
+
+
+async def broadcast_players(room_id):
+    players = rooms[room_id]["players"]
+    for ws in rooms[room_id]["sockets"]:
+        await ws.send(json.dumps({
+            "type": "PLAYERS_UPDATE",
+            "data": { "players": players }
+        }))
+
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", PORT):
